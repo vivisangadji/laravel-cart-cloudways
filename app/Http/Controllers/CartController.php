e1ad0b6641a00c82eb30e7fb83abee614429fdbc
vivi <vivisangadji06@gmail.com>
2022-11-08 02:39:11 +0800

    create update quantity

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -44,4 +44,15 @@ class CartController extends Controller
             return redirect()->back()->with('success','Product deleted successfully');
         }
     }
+
+    public function update(Request $request)
+    {
+        if($request->id && $request->quantity){
+            $cartItems = session()->get('cartItems');
+            $cartItems[$request->id]["quantity"] = $request->quantity;
+            session()->put('cartItems', $cartItems);
+        }
+
+        return redirect()->back()->with('success', 'Product added to cart!');
+    }
 }
